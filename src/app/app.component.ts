21a8b5d5e79382c86c23cbf29f4c9109d2ae40c3
Decolastic <110244042+Decolastic@users.noreply.github.com>
2023-11-18 00:47:43 -0600

    primeros cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['../demo-styling.css']
 })
 export class AppComponent {
-  title = 'angular-quickstart';
+  title = 'Boomble Party';
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+darkMode: 'media',
+
+content: [
+
+"./src/**/*.{html,ts,js}",
+
+"./node_modules/flowbite/**/*.js"
+],
+
+theme: {
+
+extend: {},
+
+},
+plugins: [
+
+require('flowbite/plugin')
+],
+
+}
